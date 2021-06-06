ca10a15259ae98328a4ab5272bb9a8265a5f2483
slezakt <info@tomasslezak.cz>
2021-06-06 18:58:50 +0200

    form

--- pages/form.vue
+++ pages/form.vue
@@ -1,9 +1,10 @@
 <template>
 	<div>
-		<form method="POST" data-netlify="true">
+		<form name="contact" method="POST" data-netlify="true">
 			<label for="">Label</label>
-			<input type="text">
+			<input type="text" name="name">
 			<div data-netlify-recaptcha="true"></div>
+			<input type="hidden" name="form-name" value="contact" />
 			<input type="submit" value="Odeslat">
 		</form>
 	</div>
